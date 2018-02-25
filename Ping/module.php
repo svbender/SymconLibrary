504c9c97cf73961f44d3450f8eab57cf86c38d52
Sven Bender <susouke@gmail.com>
2018-02-25 15:00:20 +0100

    Added new module for ping

---
+++ Ping/module.php
@@ -0,0 +1,37 @@
+<?
+    class Ping extends IPSModule {
+        public function single($ip, $timeout = 1000) {
+            return Sys_Ping($ip, $timeout);
+        }
+
+        public function multi(Array $ipList) {
+            $result = [];
+            foreach ($ipList as $name => $ip) {
+                $result[$name] = $this->single($ip);
+            }
+            return $result;
+        }
+
+        public function list($categoryId, Array $ipList, $profileName = '~Presence') {
+            $result = $this->multi($ipList);
+            foreach ($result as $name => $status) {
+
+                $varId = @IPS_GetVariableIDByName($name, $categoryId);
+                if (!$varId) {
+                    $varId = IPS_CreateVariable(0);
+                    IPS_SetName($varId, $name);
+                    IPS_SetParent($varId, $categoryId);
+                    if ($profileName) {
+                        IPS_SetVariableCustomProfile($varId, $profileName);
+                    }
+                }
+                $currentValue = GetValueBoolean($varId);
+                if ($currentValue != $response) {
+                    SetValueBoolean($varId, $response);
+                }
+                $result[$name] = $response;
+            }
+            return $result;
+        }
+    }
+?>
